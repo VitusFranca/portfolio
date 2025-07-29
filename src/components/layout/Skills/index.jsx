b44453cea0c791f53e461e176472ec23346e8fed
Vitor <vitusfranca@gmail.com>
2025-07-29 00:56:26 -0300

    ajustes

--- src/components/layout/Skills/index.jsx
+++ src/components/layout/Skills/index.jsx
@@ -1,5 +1,5 @@
 import { FaReact, FaCss3Alt, FaJsSquare, FaHtml5, FaGitSquare, FaGithubSquare  } from 'react-icons/fa';
-
+import { Code, CodeXml } from 'lucide-react';
 import styles from './styles.module.css';
 
 
@@ -9,7 +9,7 @@ function Skills ( { scrollRef }) {
             <div className={styles.conteiner_atuacao}>
                 <h1>Professional Action</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Qui suscipit similique quo corrupti esse dolores ab ea labore aut ex laudantium cum id,<br/> accusamus molestias cupiditate delectus placeat corporis provident.</p>
-                <div className={styles.conteiner_item}><FaCss3Alt className={styles.icon} /><h2></h2></div>
+                <div className={styles.conteiner_item}><CodeXml className={styles.icon} /><h2></h2></div>
             </div>
             <div className={styles.conteiner_icons}>
                 <div className={styles.conteiner_item}><FaHtml5 className={styles.icon} /><h2>HTML</h2></div>
